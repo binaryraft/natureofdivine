f4c9c91b72cae62108292d7352b0135be4e0fadd
binaryraft <alfu46991@gmail.com>
2025-11-07 13:05:40 +0530

    sitemap infinite loop fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   changefreq: 'weekly',
   priority: 0.8,
   sitemapSize: 5000,
+
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://www.natureofthedivine.com/sitemap.xml',
